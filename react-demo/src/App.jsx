691ea63389241290cece9837896061e54967fb57
IreneMonlleo <irene.monlleo@gmail.com>
2022-05-23 17:09:25 +0200

    create component link

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header';
-
+import Link from './Components/Link';
 
 function App() {
   
   return (
     <div className="App">
-      <Routes>
+      <nav>
+        <Link to ="/">Home</Link>
+        <Link to ="/grafic">Diseño Gráfico</Link>
+      </nav>
+      {/* <Routes>
 				// Cuando la url sea '/' se visualizará el elemento HOME
         <Route path="/" element={<Home />} />
-		
         <Route path="/grafic" element={<Grafic />}></Route>
 				// Cuando la url sea cualquier otra cosa, se visualizará NOMATCH
         <Route path="/ux" element={<Ux />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
---
+++ react-demo/src/components/Link.jsx
@@ -0,0 +1,4 @@
+
+
+export default function Links(props) {
+}
