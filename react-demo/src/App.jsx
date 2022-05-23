9b99b6b5a8d8ecd32c32443f6f7563944a918371
IreneMonlleo <irene.monlleo@gmail.com>
2022-05-23 13:26:59 +0200

    intall react-router-dom

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Header from './Components/header';
+import Header from './Components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      hola
-      <Header></Header>
+  hola gente
+      <Header className="Diseño Gráfico"></Header>
     </div>
   );
 }
--- react-demo/src/Components/header.jsx
+++ react-demo/src/Components/header.jsx
@@ -1,7 +1,7 @@
+
 export default function Header(props){
     return 
     <header className="header">
-        <img>{props.img}</img>
          <h1> {props.name} </h1>;
     </header>
    
