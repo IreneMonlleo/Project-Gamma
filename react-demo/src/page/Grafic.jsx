d0e4c1cbf23b833a76fcfb6707a6eb089cac472d
IreneMonlleo <irene.monlleo@gmail.com>
2022-05-24 12:45:04 +0200

    structure about
    about is inside the home page

--- react-demo/src/page/Grafic.jsx
+++ react-demo/src/page/Grafic.jsx
@@ -1,13 +1,17 @@
-import Header from "../components/Header"
+import React from "react";
+import Header from "../components/Header";
+import ManchaGrafic from '../img/ManchaGrafic.png';
+
 
 
 
 
 export default  function Grafic() {
-    const description="Un diseñador es aquel que sabe un poquito de todo, pues cada proyecto es diferente y requiere de una investigación y análisis previo. De este modo, podemos encontrar la solución más adecuada."
+    const description= `Un diseñador es aquel que sabe un poquito de todo, pues cada proyecto es diferente 
+    y requiere de una investigación y análisis previo. De este modo, podemos encontrar la solución más adecuada.`
     
     return <div className="Grafico">
-        <Header title="Diseño Gráfico" img="./img/ManchaGrafico.png" description={description}></Header>
+        <Header title="Diseño Gráfico" img={ManchaGrafic} description={description}></Header>
         <main>
             
         </main>
--- react-demo/src/page/Home.jsx
+++ react-demo/src/page/Home.jsx
@@ -1,10 +1,51 @@
+import React from "react";
 import './Home.css';
+import Mon_lleo from '../img/Mon_lleo.gif';
+import Irene from '../img/IreneMonlleo.png';
+
+
+
 
 
 export default function Home(props) {
-  
 
-    return <section className="beginning">
-    <h1 className='title__beginning'>MON <br></br>LLE</h1>  
-    </section>
+
+    return ( 
+    <div>
+      <section className='beginning'>
+          <h1 className='title__beginning'>MON <br></br>LLE</h1> 
+          <img className="gif"  src={Mon_lleo} alt=''/>
+      </section>
+
+      <main className="home">
+        <section>
+          <p className="home__text">Hola!!<br></br> 
+              Soy <b>Irene Monlleó</b> y soy una profesional multidisciplinar por lo que eres libre de elegir que trabajos quieres ojear </p>
+          <h3 className="quest">¿Que estas buscando?</h3>
+          <div className="quest__buttons">
+            <button></button>
+            <button></button>
+            <button></button>
+          </div>
+        </section>
+        <article className="about">
+          <img className="about__image" src={Irene} alt="Irene Monlleo"></img>
+          <div className="about__container-text">
+            <h1 className="about__title">Bienvenidos a mi Portfolio</h1>
+            <p className="about__description">Como ya sabeis me llamo Irene Monlleo. Estoy graduada en Bellas Artes en la Universidad Politécnica de Valencia. Seguidamente 
+              me especialice en Grafica Publicitaria en la EASD de Castellón y por último he dado el salto al mundo digital de la mano de ESAT 
+              Valencia con su postgrado especializado en UX/UI, Marketin digital y Desarrollo Web Front-end.</p>
+            <button className="buttons">Contactar</button>
+          </div>
+        </article>
+        
+
+
+      </main>
+    </div>
+    
+    
+
+
+    );
   }
