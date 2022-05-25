6a5609dca47f44c7b9b5c9f3cfbffbffc39186de
IreneMonlleo <irene.monlleo@gmail.com>
2022-05-25 17:06:22 +0200

    variables colors

--- react-demo/src/components/Footer.jsx
+++ react-demo/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import './Footer.css';
 export default function Footer(props) {
   
 
-    return <div>
+    return <div className='footer'>
       <img className="footer__logo" src={props.img} alt="logo Monlleo" />
-      <h2 className="header__title"> { props.name }</h2>
+      <h2 className="footer__title"> { props.name }</h2>
       <p className="footer__email">{props.email}</p>
       <p className="footer__rrss">{props.rrss}</p>
     </div>
