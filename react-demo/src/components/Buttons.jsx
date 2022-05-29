8fd2870bab4c7515104640e37b6d6c6874985b12
IreneMonlleo <irene.monlleo@gmail.com>
2022-05-30 00:20:58 +0200

    fix style buttons

--- react-demo/src/components/Buttons.jsx
+++ react-demo/src/components/Buttons.jsx
@@ -4,29 +4,60 @@ const SecondButton = `
 
     border-color: var(--color-bg); 
     color:var(--color-bg); 
-    background-color:var(--color-home);
     
         &:hover{
-            background-color: var(--color-bg);
             color:var(--color-typography);
         }
+
+        &::before{
+            box-shadow: inset 0 0 0 20px var(--color-bg);
+        }
+        
+        
 `;
 
 export const Button = styled.button`
+	cursor: pointer;
+	position: relative;
+	display: inline-block;
 	padding: 12px 24px;
     margin: 12px;
 	border: 3px solid var(--color-home);
-    border-radius: 50px;
-    background-color: var(--color-bg);
-    color: var(--color-home);
-   
+	border-radius: 50px;
+    font-weight: bold;
+	overflow: hidden;
+    color:var(--color-home);
+    background-color: transparent;
 
         &:hover{
-            background-color: var(--color-home);
-            color: var(--color-bg);
+            color:var(--color-bg);
         }
 
- 
+        &::before, &::after{
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 100%;
+            width: 100%;
+            transition: all 0.25s ease;
+            border-radius: 50px;
+        }
+            &::before{
+                border-radius: 50px;
+                margin-left: -100%;
+                box-shadow: inset 0 0 0 20px var(--color-home);
+                z-index:-1;
+                
+            }
+            
+            &:hover::before{
+                margin-left: 0;
+                
+            }
+    }
+
+    
 
 
      ${props => props.secondary ? SecondButton :''};
