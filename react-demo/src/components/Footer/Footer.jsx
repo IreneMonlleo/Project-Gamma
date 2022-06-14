4233a6f7dbc3fe35e9dc0dd6965b83ec10d4ef9a
IreneMonlleo <irene.monlleo@gmail.com>
2022-06-14 10:53:20 +0200

    fix footer

--- react-demo/src/components/Footer/Footer.jsx
+++ react-demo/src/components/Footer/Footer.jsx
@@ -5,10 +5,7 @@ export default function Footer(props) {
 
     return <div className='footer' id="contact">
       <img className="footer__logo" src={props.img} alt="logo Monlleo" />
-      <h2 className="footer__title">
-        <span className="title--top">Puedes contactar</span>
-        <span>conmigo en:</span>
-      </h2>
+      <h2 className="footer__title">Puedes contactar <br></br>conmigo en:</h2>
       <div className="contact">
         <a className="footer__email" href="mailto:irene.monlleo@gmail.com" >irene.monlleo@gmail.com</a>
         <a className="footer__rrss" href="https://www.instagram.com/mon_lleo/">@Mon_lleo</a>
