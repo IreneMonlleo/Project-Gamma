d6bd8748268b9700acdeff6f118e9b1f58afa6d6
IreneMonlleo <irene.monlleo@gmail.com>
2022-06-14 14:47:22 +0200

    fix header background project page

--- react-demo/src/components/CardProject/CardProject.jsx
+++ react-demo/src/components/CardProject/CardProject.jsx
@@ -4,14 +4,14 @@ export default function CardProjects(props) {
   
   return(
     <article className="container-card">
-    <div className="container-img-card">
-        <img className="img-card" src={props.img1} alt={props.alt1}></img>
-        <img className="img-card grap" src={props.img2} alt={props.alt2}></img> 
-    </div>
-    <h3 className='titles-section'>{props.title}</h3>
-    <p className="text-card left">{props.text}</p>
-    <p className="text-card left">{props.text1}</p>
-    <p className="text-card left">{props.text2}</p>
+      <div className="container-img-card">
+          <img className="img-card" src={props.img1} alt={props.alt1}></img>
+          <img className="img-card grap" src={props.img2} alt={props.alt2}></img> 
+      </div>
+      <h3 className='titles-section'>{props.title}</h3>
+      <p className="text-card left">{props.text}</p>
+      <p className="text-card left">{props.text1}</p>
+      <p className="text-card left">{props.text2}</p>
     </article>
   )
 } 
--- react-demo/src/components/Header/Header.jsx
+++ react-demo/src/components/Header/Header.jsx
@@ -1,15 +1,79 @@
-// import { useParams } from 'react-router-dom';
-import './Header.css';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+// import './Header.css';
 
 export default function Header(props) {
-// let params = useParams();
+let params = useParams();
 
-  return <header className="header">
+  return <WrapperHeader Ux={params.category == ['ux-ui']}>
     <div className='header__container'>
       <img className='header__image' src={ props.img } alt="Mancha corporativa" />
       <h1 className='left'>{ props.title }<br></br>{ props.secondtitle }
       </h1>
     </div>
     <p className="header__description"> { props.description }</p>
-  </header>
+  </WrapperHeader>
 }
+
+export const WrapperHeader = styled.header`
+.header__container{
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+  margin-top: 11vh;
+
+    &::before{
+      content: "";
+      background-color: ${({ Ux }) => (Ux ? "var(--color-header,#DD723A)"  : "var(--color-header,#A9B59D)")}; //Import;
+      position: absolute;  
+      top: 0px;
+      left: 0px;
+      bottom:50vh;
+      right: 0px;
+      animation: slid-header 1s ease-in forwards;
+    }
+}
+  
+  @keyframes slid-header {
+      0% {
+          top:0;
+          bottom:100%
+          
+      }
+     100% {
+          bottom:54vh ;
+         
+      }
+    } 
+
+  .header__image{
+      height: 13vh;
+      margin:12px;
+  }
+
+  .header__description{
+      margin:48px 25vw;
+      text-align: left;
+      
+  }
+
+  @media all and (min-width:700px){
+
+      .header__container{
+          margin-top: 30vh;
+
+          &::before{
+            margin: 0 80px;
+            /* bottom: 50vh; */
+          }
+      }
+          
+  
+      .header__description{
+          margin:102px 25vw;        
+      }
+  
+  
+  }
+
+`
