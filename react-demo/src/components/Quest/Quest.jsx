d12b4ef35559a355e9515c3518547446bcb24179
IreneMonlleo <irene.monlleo@gmail.com>
2022-06-15 20:21:19 +0200

    change text quets

--- react-demo/src/components/Quest/Quest.jsx
+++ react-demo/src/components/Quest/Quest.jsx
@@ -11,9 +11,9 @@ export default function Quest(props) {
     return ( 
         <section className="quest__container">
           <div className='container-text-quest'>
-            <p className="quest__text">Hola!!<br></br> 
-                Soy <b>Irene Monlleó</b> y soy una profesional multidisciplinar por lo que eres libre de elegir que trabajos quieres ojear </p>
-            <h3 className="quest">¿Que estas buscando?</h3>
+            <p className="quest__text">¡¡Hola!!<br></br> 
+            Me llamo  <b>Irene Monlleó</b>. ¿Cuál de mis campos profesionales encaja mejor contigo? </p>
+            <h3 className="quest">Pincha para descubrir mis proyectos</h3>
           </div>
           <div className="quest__buttons">
          
